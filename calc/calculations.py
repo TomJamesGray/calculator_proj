80f0da918331c66ba8928608e1f1586c6fd9dfb3
TomJamesGray <tomg77@yahoo.co.uk>
2017-07-11 18:07:59 +0100

    Added regex to split between functions and added function to evaluate the reverse polish notation line

--- calc/calculations.py
+++ calc/calculations.py
@@ -16,42 +16,50 @@ functions = {
     "+":{
         "n":2,
         "func": lambda x,y: x+y,
-        "level":2
+        "level":2,
+        "regex_name":"\+"
     },
     "-":{
         "n":2,
         "func": lambda x,y: x-y,
-        "level":2
+        "level":2,
+        "regex_name":"-"
     },
     "*":{
         "n":2,
         "func":lambda x,y: x*y,
-        "level":3
+        "level":3,
+        "regex_name":"\*"
     },
     "/":{
         "n":2,
         "func":lambda x,y:x/y,
-        "level":3
+        "level":3,
+        "regex_name":"\/"
     },
     "^":{
         "n":2,
         "func":lambda x,y:x**y,
-        "level":4
+        "level":4,
+        "regex_name":"\^"
     },
     "(":{
         "n":0,
         "func":None,
-        "level":1
+        "level":1,
+        "regex_name":"\("
     },
     ")":{
         "n":0,
         "func":None,
-        "level":1
+        "level":1,
+        "regex_name":"\)"
     },
-    "sin(":{
+    "sin":{
         "n":1,
         "func":lambda x:math.sin(x),
-        "level":5
+        "level":5,
+        "regex_name":"sin"
     }
 }
 
@@ -66,55 +74,81 @@ def parse_line(calc_line,ans):
     """
     global functions
     f_stack = []
-    rpn_line = ""
-    # Temporarily just split on sin
-    f_line = re.split("(sin)",calc_line[0])
+    rpn_line = []
+    # Construct regex to split on all operations
+    regex_names = []
+    for f_name,func in functions.items():
+        regex_names.append(func["regex_name"])
+
+    f_line = re.split("({})".format("|".join(regex_names)),calc_line[0])
+
     logger.info("Eval {}".format(f_line))
 
-    for a in f_line:
-        if a == "":
-            pass
-        elif a == "sin":
-            f_stack.append(a)
+    for c in f_line:
+        if c == "":
+            continue
+        logger.debug("Using {}".format(c))
+        if c in functions:
+            # Current item is a function
+            if c == "(":
+                logger.debug("Adding ( to f_stack")
+                f_stack.append(c)
+            elif c == ")":
+                logger.debug("Closing bracket")
+                while f_stack[-1] != "(":
+                    rpn_line.append(f_stack.pop())
+                f_stack.pop()
+                logger.debug("f_stack after ')': {}".format(f_stack))
+
+            elif len(f_stack) == 0:
+                logger.debug("Appending function {} to empty f_stack".format(c))
+                f_stack.append(c)
+            elif functions[f_stack[-1]]["level"] < functions[c]["level"]:
+                logger.debug("Appending function {} to f_stack".format(c))
+                f_stack.append(c)
+            else:
+                try:
+                    while functions[f_stack[-1]]["level"] >= functions[c]["level"]:
+                        logger.debug("Adding {} to rpn_line as higher than {}".format(f_stack[-1],c))
+                        rpn_line.append(f_stack.pop())
+                except IndexError:
+                    # f_stack is empty
+                    pass
+                f_stack.append(c)
+                logger.debug("Added {} to f_stack, now {}".format(c,f_stack))
+            logger.debug("f_stack at {}".format(f_stack))
+
         else:
-            for c in a:
-                logger.debug("Using {}".format(c))
-                if c in functions:
-                    # Current character is a function
-                    if c == "(":
-                        logger.debug("Adding ( to f_stack")
-                        f_stack.append(c)
-                    elif c == ")":
-                        logger.debug("Closing bracket")
-                        while f_stack[-1] != "(":
-                            rpn_line += f_stack.pop()
-                        f_stack.pop()
-                        logger.debug("f_stack after ')': {}".format(f_stack))
-
-                    elif len(f_stack) == 0:
-                        logger.debug("Appending function {} to empty f_stack".format(c))
-                        f_stack.append(c)
-                    elif functions[f_stack[-1]]["level"] < functions[c]["level"]:
-                        logger.debug("Appending function {} to f_stack".format(c))
-                        f_stack.append(c)
-                    else:
-                        try:
-                            while functions[f_stack[-1]]["level"] >= functions[c]["level"]:
-                                logger.debug("Adding {} to rpn_line as higher than {}".format(f_stack[-1],c))
-                                rpn_line += f_stack.pop()
-                        except IndexError:
-                            # f_stack is empty
-                            pass
-                        f_stack.append(c)
-                        logger.debug("Added {} to f_stack, now {}".format(c,f_stack))
-
-                else:
-                    # Current character is an operand
-                    rpn_line += c
-
+            # Current item is an operand
+            rpn_line.append(c)
 
     while f_stack != []:
-        rpn_line += f_stack.pop()
+        rpn_line.append(f_stack.pop())
 
     logger.info("RPN line at end of parsing: {}".format(rpn_line))
-    return 5
+    val = eval_rpn(rpn_line)
+    return val
+
+def eval_rpn(rpn_line):
+    global functions
+    eval_stack = []
+    for c in rpn_line:
+        if c in functions:
+            logger.debug("Evaluating function {}".format(c))
+            func = functions[c]
+            args = []
+            for i in range(0,func["n"]):
+                # Retrieve required amount of arguments
+                args.append(float(eval_stack.pop()))
+            # Reverse args so first argument would be towards bottom of stack
+            args = args[::-1]
+            logger.debug("Using args: {}".format(args))
+            val = func["func"](*args)
+            eval_stack.append(val)
+            logger.debug("Adding value from function {} to stack".format(val))
+        else:
+            logger.debug("Adding {} to eval_stack".format(c))
+            eval_stack.append(c)
+            logger.debug("eval_stack at {}".format(eval_stack))
+
+    return eval_stack[0]
